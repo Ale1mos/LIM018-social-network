80ba424f636071a8c3d5ae7fc44a5e5b0a493a6c
Tu-username <alejandramosqueirazapata1@gmail.com>
2022-08-02 20:44:29 -0500

    spa

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+export const home = () => {
+  const homeDiv = document.createElement('div')
+  const buttonRegister = document.createElement('button')
+  const buttonLogin = document.createElement('button')
+
+  buttonRegister.textContent= 'Regístrate'
+  buttonLogin.textContent= 'Inicia Sesión'
+
+  homeDiv.appendChild(buttonRegister)
+  homeDiv.appendChild(buttonLogin)
+
+  return homeDiv;
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+import { home } from './components/home.js';
+import { login } from './components/login.js';
+import { register } from './components/register.js';
+// import { myFunction } from './lib/index.js';
 
-myFunction();
+const rootDiv = document.getElementById("root")
+
+// myFunction();
+const routes ={
+  '/':home,
+  'register/':register,
+  'register/':login,
+}
+
+const component = routes[window.location.pathname]
+rootDiv.appendChild(component())
