fc18bdb0bcfc956af03c3012c9bc4d8725b1f7be
Tu-username <alejandramosqueirazapata1@gmail.com>
2022-08-03 11:42:43 -0500

    ramas

--- src/main.js
+++ src/main.js
@@ -1,18 +1,25 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { home } from './components/home.js';
-import { login } from './components/login.js';
-import { register } from './components/register.js';
-// import { myFunction } from './lib/index.js';
+// import { home } from './components/home.js';
+// import { login } from './components/login.js';
+// import { register } from './components/register.js';
+// // import { myFunction } from './lib/index.js';
 
-const rootDiv = document.getElementById("root")
+// const rootDiv = document.getElementById("root")
 
-// myFunction();
-const routes ={
-  '/':home,
-  'register/':register,
-  'register/':login,
+// // myFunction();
+// const routes ={
+//   '/':home,
+//   'register/':register,
+//   'register/':login,
+// }
+
+// const component = routes[window.location.pathname]
+// rootDiv.appendChild(component())
+
+const init = () => {
+  window.addEventListener('hashchange' , () => console.log(window.location.hash))
 }
 
-const component = routes[window.location.pathname]
-rootDiv.appendChild(component())
+window.addEventListener('load', init)
+
